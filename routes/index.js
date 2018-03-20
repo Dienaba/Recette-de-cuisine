e3ec97ff5f9dddeb00200ef4477b8dc451680ce3
Dienaba Camara <camara.dienaba@gmail.com>
2018-03-20 19:12:27 +0100

    ajout api food2fork + affichage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Recette de Cuisine' });
+  res.render('index', { title: "Des'Idées", body : {recipes: [] }  });
 });
+
+router.post('/', function(req, res, next) {
+  recipes = [];
+  request('http://food2fork.com/api/search?key=f48cc78e3a40ae7e2cae2e919ed329f8&q=' + req.body.recherche,
+    function (error, response, body) {
+      res.render('index', { title: "Des'Idées", recipes: body });
+  });
+});
+
 
 module.exports = router;
